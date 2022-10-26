a1dcba7dc597461a8ed663ec90d272dfdedbe9fc
Andrew <bigfrendyoff@gmail.com>
2022-10-26 13:50:42 +0300

    Adding ballons (trying...)

--- VDNKHLarsan/static/js/map.js
+++ VDNKHLarsan/static/js/map.js
@@ -1,4 +1,7 @@
+
+
 var myMap;
+
 
 
 ymaps.ready(init);
@@ -8,15 +11,36 @@ function init () {
     myMap = new ymaps.Map('map', {
         center: [55.832135, 37.628041],
         zoom: 14,
-        controls: ['smallMapDefaultSet']
-    },{
-        restrictMapArea: [
-            [55.822535, 37.609675],
-            [55.843460, 37.642077]
-        ]},
+        // controls: ['smallMapDefaultSet']
+    },
+        // {
+        // restrictMapArea: [
+        //     // [55.822535, 37.609675],
+        //     // [55.843460, 37.642077]
+        // ]},
         {
         searchControlProvider: 'yandex#search'}
     );
+
+
+    for (var place in Object.values(places)) {
+        console.log(place.geometry.coordinates.reverse())
+
+
+    }
+
+    // foreach(places, (index, place) => {
+    //     console.log(place.geometry.coordinates.reverse())
+    //     myMap.geoObjects.add(new ymaps.Placemark(place.geometry.coordinates.reverse(),{
+    //         balloonContentHeader: "Балун метки",
+    //         balloonContentBody: "Содержимое <em>балуна</em> метки",
+    //         balloonContentFooter: "Подвал",
+    //         hintContent: "Хинт метки"
+    //     }))
+    // })
+
+
+
     myMap.controls.remove("trafficControl")
     myMap.controls.remove("fullscreenControl")
     myMap.controls.remove("typeSelector")
