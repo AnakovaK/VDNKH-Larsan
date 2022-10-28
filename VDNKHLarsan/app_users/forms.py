fc297cdb44b35465ce658fcc6d4fccb7077250c7
demid-dk <demid.kopan@gmail.com>
2022-10-29 00:31:56 +0300

    Templates improved

--- VDNKHLarsan/app_users/forms.py
+++ VDNKHLarsan/app_users/forms.py
@@ -1,11 +1,19 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
+class LoginForm(AuthenticationForm):
+
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form__input'
+
+
 class SignUpForm(UserCreationForm):
-    patronymic = forms.TextInput()
-    is_moscowian = forms.CheckboxInput()
+    patronymic = forms.CharField(widget=forms.TextInput())
+    is_moscowian = forms.ChoiceField(widget=forms.CheckboxInput())
 
     class Meta:
         model = User
@@ -13,6 +21,20 @@ class SignUpForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form__input'
+
+
+class ProfileForm(UserCreationForm):
+    patronymic = forms.CharField(widget=forms.TextInput())
+    is_moscowian = forms.ChoiceField(widget=forms.CheckboxInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form__input'
 
--- VDNKHLarsan/app_users/views.py
+++ VDNKHLarsan/app_users/views.py
@@ -6,15 +6,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import FormView, UpdateView, DetailView
 
-from .forms import SignUpForm
+from .forms import SignUpForm, LoginForm, ProfileForm
 
 
 class SignUpView(FormView):
     template_name = 'app_users/sign-up.html'
     form_class = SignUpForm
-
-    def get_success_url(self):
-        return reverse('profile', kwargs={'pk': self.object.id})
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
         user = form.save()
@@ -22,12 +20,13 @@ class SignUpView(FormView):
         raw_password = form.cleaned_data.get('password1')
         user = authenticate(username=username, password=raw_password)
         login(self.request, user)
-        return HttpResponseRedirect(reverse('home'))
+        return HttpResponseRedirect(self.success_url)
 
 
 class MyLoginView(LoginView):
     template_name = 'app_users/login.html'
     next_page = reverse_lazy('home')
+    form_class = LoginForm
 
 
 class MyLogoutView(LogoutView):
@@ -45,7 +44,7 @@ class ProfileUpdateView(UpdateView):
     template_name = 'app_users/redact.html'
     model = User
     context_object_name = 'profile'
-    fields = ("username", "first_name", "last_name", "email")
+    form_class = ProfileForm
 
     def get_success_url(self):
         return reverse('profile', kwargs={'pk': self.object.id})
