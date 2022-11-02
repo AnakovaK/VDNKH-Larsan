b844c6c163c972fcb4cdddf98091a4f9ae4c208f
demid-dk <demid.kopan@gmail.com>
2022-11-02 22:24:43 +0300

    Fixed containers' size

--- VDNKHLarsan/app_main/views.py
+++ VDNKHLarsan/app_main/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'app_main/index.html'
+
+
+class MapView(TemplateView):
+    template_name = 'app_main/map.html'
