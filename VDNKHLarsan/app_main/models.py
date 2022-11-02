52c11291b221601626ac4bf81e47fc6b667e7b7c
demid-dk <demid.kopan@gmail.com>
2022-11-02 22:56:06 +0300

    Added route and place models (work in progress)

--- VDNKHLarsan/app_main/models.py
+++ VDNKHLarsan/app_main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+
+
+class Route(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    places = models.ManyToManyField(Place, related_name='route', verbose_name='Места')
--- VDNKHLarsan/app_main/urls.py
+++ VDNKHLarsan/app_main/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
-    path('/map/', views.MapView.as_view(), name='map'),
+    path('map/', views.MapView.as_view(), name='map'),
+    path('map/routes', views.RouteListView.as_view(), name='route-list'),
+    path('map/routes/<int:pk>', views.RouteDetailView.as_view(), name='route-detail'),
 ]
