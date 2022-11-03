59a161c2a81fae42a1ae2442fd65a94c1f0973b2
demid-dk <demid.kopan@gmail.com>
2022-11-03 12:16:52 +0300

    Improved route and place models

--- VDNKHLarsan/app_main/urls.py
+++ VDNKHLarsan/app_main/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('map/', views.MapView.as_view(), name='map'),
-    path('map/routes', views.RouteListView.as_view(), name='route-list'),
-    path('map/routes/<int:pk>', views.RouteDetailView.as_view(), name='route-detail'),
+    path('routes/', views.RouteListView.as_view(), name='route-list'),
+    path('routes/<slug:slug>/', views.RouteDetailView.as_view(), name='route-detail'),
 ]
--- VDNKHLarsan/app_main/views.py
+++ VDNKHLarsan/app_main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 
+from .models import Route
+
 
 class IndexView(TemplateView):
     template_name = 'app_main/index.html'
@@ -12,8 +14,11 @@ class MapView(TemplateView):
 
 class RouteListView(ListView):
     template_name = 'app_main/map/route-list.html'
+    model = Route
+    context_object_name = 'routes'
 
 
 class RouteDetailView(DetailView):
     template_name = 'app_main/map/route-detail.html'
-
+    model = Route
+    context_object_name = 'route'
