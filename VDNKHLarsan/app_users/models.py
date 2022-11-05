dbcacb97a81a421b9ce5595a04a5a87368ec5b20
demid-dk <demid.kopan@gmail.com>
2022-11-05 13:48:34 +0300

    Models fixed

--- VDNKHLarsan/app_users/models.py
+++ VDNKHLarsan/app_users/models.py
@@ -20,24 +20,17 @@ class User(AbstractUser):
     def get_tags(self):
         return self.tags
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='Пользователь')
-#     patronymic = models.CharField(max_length=55, verbose_name='Отчество')
-#     born = models.DateField(verbose_name='Дата рождения')
-#
-#     @property
-#     def first_name(self):
-#         return self.user.first_name
-#
-#     @property
-#     def last_name(self):
-#         return self.user.last_name
-#
-#     @property
-#     def age(self):
-#         from datetime import date
-#         today = date.today()
-#         return today.year - self.born.year - ((today.month, today.day) < (self.born.month, self.born.day))
-#
-#     def __str__(self):
-#         return self.first_name + " " + self.last_name + " " + self.patronymic
+
+class RoutePoint(models.Model):
+    point = models.IntegerField()
+
+    def __str__(self):
+        return str(self.point)
+
+
+class CustomRoute(models.Model):
+    route_points = models.ManyToManyField(RoutePoint)
+
+    def __str__(self):
+        all_points = ", ".join(str(seg) for seg in self.route_points.all())
+        return "{}".format(all_points)
