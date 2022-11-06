c8e684f614f081d91947b310a96bff4842933e96
Andrew <bigfrendyoff@gmail.com>
2022-11-06 23:42:22 +0300

    g

--- VDNKHLarsan/VDNKHLarsan/settings.py
+++ VDNKHLarsan/VDNKHLarsan/settings.py
@@ -143,3 +143,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'app_users.User'
+
+CSRF_TRUSTED_ORIGINS = ['https://vdnhlarsan.herokuapp.com/','https://127.0.0.1']
