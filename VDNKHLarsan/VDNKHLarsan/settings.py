34a69104a547a9aa26f18b3659f1256c836d9867
Andrew <bigfrendyoff@gmail.com>
2022-11-06 22:22:58 +0300

    WIP

--- VDNKHLarsan/VDNKHLarsan/settings.py
+++ VDNKHLarsan/VDNKHLarsan/settings.py
@@ -16,7 +16,7 @@ import os
 from boto.s3.connection import S3Connection
 
 load_dotenv()
-
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
